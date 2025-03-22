fd10a1429dc5e8a81ebed304fa3b034da337029f
LOHITHK33 <lohithkasukurthi33@gmail.com>
2025-03-22 18:49:46 +0530

    background color change

--- my-react-app/src/NotFound.jsx
+++ my-react-app/src/NotFound.jsx
@@ -21,14 +21,6 @@ export default function NotFound() {
 
   return (
     <>
-      {/*
-        This example requires updating your template:
-
-        ```
-        <html class="h-full">
-        <body class="h-full">
-        ```
-      */}
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className="text-base font-semibold text-indigo-600">404</p>
@@ -45,6 +37,8 @@ export default function NotFound() {
             >
               Go back to home
             </Link>
+          </div>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
             <a href="#" className="text-sm font-semibold text-gray-900">
               Contact support <span aria-hidden="true">&rarr;</span>
             </a>
