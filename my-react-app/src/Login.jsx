1cf4bb3334fb9eab02fece4aa96ccf481bcbdad4
LOHITHK33 <lohithkasukurthi33@gmail.com>
2025-03-22 18:12:57 +0530

    css change

--- my-react-app/src/Login.jsx
+++ my-react-app/src/Login.jsx
@@ -23,11 +23,12 @@ export default function Example() {
 
   return (
     <>
-      <div className="flex min-h-full h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8">
+      <div className="flex min-h-full h-screen flex-1 flex-col justify-center items-center px-6 py-12 lg:px-8 mx-auto">
+
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Lazy Panda"
+            src="./lp.png"
             className="mx-auto h-10 w-auto"
           />
           <h2 className="mt-10 text-center text-2xl font-bold tracking-tight text-gray-900">
